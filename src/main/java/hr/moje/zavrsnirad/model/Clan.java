0a5ef3fc524fa14c48c6a34719b9717aabaff1ae
Boris Lasović <57643982+BorisLos@users.noreply.github.com>
2020-04-21 18:42:40 +0200

    Utorak

--- src/main/java/hr/moje/zavrsnirad/model/Clan.java
+++ src/main/java/hr/moje/zavrsnirad/model/Clan.java
@@ -29,60 +29,7 @@ public class Clan extends Osoba{
     //private Kategorija kategorija;
     
     
-    /**
-    public Kategorija getKategorija() {
-        return kategorija;
-    }
-    
-    public void setKategorija(Kategorija kategorija) {
-        this.kategorija = kategorija;
-    }
-    
-*/
-    /*
-    public String getIme() {
-        return ime;
-    }
 
-    public void setIme(String ime) {
-        this.ime = ime;
-    }
-
-    public String getPrezime() {
-        return prezime;
-    }
-
-    public void setPrezime(String prezime) {
-        this.prezime = prezime;
-    }
-/*
-    public String getSpol() {
-        return spol;
-    }
-
-    public void setSpol(String spol) {
-        this.spol = spol;
-    }
-
-    /*
-    public String getDatum_rodjenja() {
-        return datum_rodjenja;
-    }
-
-    public void setDatum_rodjenja(String datum_rodjenja) {
-        this.datum_rodjenja = datum_rodjenja;
-    }
-    */
-
-/*
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-*/
     public String getUlica() {
         return ulica;
     }
